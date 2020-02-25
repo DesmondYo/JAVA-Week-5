cfe98237d7a4b4b3cf8d40434903fbc60c81aafc
Desmond Young <60267316+DesmondYo@users.noreply.github.com>
2020-02-25 15:33:07 -0700

    Update SpacedLogger.java

--- JAVA Week 5 Homework/src/SpacedLogger.java
+++ JAVA Week 5 Homework/src/SpacedLogger.java
@@ -3,11 +3,11 @@ public class SpacedLogger implements Logger {
 	
 	//6 & 7.
 	@Override
-	public void log(String AppleWatch) {
+	public void log(String Message) {
 		
 		StringBuilder log = new StringBuilder();
-		for (int i = 0; i < AppleWatch.length(); i++) {
-			log.append(AppleWatch.charAt(i) + " ");
+		for (int i = 0; i < Message.length(); i++) {
+			log.append(Message.charAt(i) + " ");
 		}
 		System.out.println(log);
 	
@@ -15,9 +15,9 @@ public class SpacedLogger implements Logger {
 	
 	//8.
 	@Override
-	public void error(String Destroy) {
-		System.out.println("ERROR: ");
-		log (Destroy);
+	public void error(String Message) {
+		System.out.print("Error: ");
+		log (Message);
 		
 	}
 
